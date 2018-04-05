32dac33ec9857c3f4cd324df1b3eb47228ca4ff8
Omar Khairy Rostom <o.rostom.93@gmail.com>
2018-04-05 21:14:28 +0200

    Added constants and config files

---
+++ app/src/main/java/com/omarkrostom/azanEdge/Config.kt
@@ -0,0 +1,12 @@
+package com.omarkrostom.azanEdge
+
+object Config {
+
+    const val PRAYER_TIMES_API = "https://aladhan.p.mashape.com/"
+    const val PRAYER_TIMES_AUTH_KEY = "ERCrApjWSjmshC8Z3dOGoe7oNFFqp1GJG8Yjsn9FF5mFLDYmhX"
+    const val LOCATION_PERMISSION: Int = 11
+    const val LOCATION_INTERVAL: Long = 10000
+    const val FASTEST_LOCATION_INTERVAL: Long = 100
+    const val PRAYER_UPDATE_INTERVAL: Long = 1000 * 60 * 60 * 24
+
+}
---
+++ app/src/main/java/com/omarkrostom/azanEdge/Constants.kt
@@ -0,0 +1,40 @@
+package com.omarkrostom.azanEdge
+
+object Constants {
+
+    const val FAJR_PRAYER = "ic_fagr"
+    const val ZUHR_PRAYER = "ic_zuhr"
+    const val ASR_PRAYER = "ic_asr"
+    const val MAGHRIB_PRAYER = "maghrib"
+    const val ISHA_PRAYER = "ic_isha"
+
+    const val DEFAULT_CITY = "Alexandria Governorate"
+    const val DEFAULT_COUNTRY = "Egypt"
+
+    const val DEFAULT_LONG = 29.9562791
+    const val DEFAULT_LAT = 31.22962649
+
+    const val APP_CITY = "city"
+    const val APP_COUNTRY = "country"
+    const val APP_LONG = "long"
+    const val APP_LAT = "lat"
+
+    const val DEFAULT_METHOD = "Egyptian General Authority of Survey"
+    const val DEFAULT_METHOD_INDEX = 2
+
+    const val AZAN_METHOD = "azan_method"
+    const val AZAN_METHOD_INDEX = "azan_method_index"
+    const val SOMETHING_WENT_WRONG: String = "Something went wrong !"
+
+    const val SAMSUNG_INTENT_FILTER =
+            "com.samsung.android.providers.context.permission.WRITE_USE_APP_FEATURE_SURVEY"
+
+    const val PRAYER_DEFAULT = "00:00"
+
+    const val HOUR_MODE: String = "hour_mode"
+    const val TWELVE_HOUR: String = "twelve_hour"
+    const val TWENTY_FOUR_HOUR: String = "twenty_four_hour"
+    const val PM: String = " PM"
+    const val AM: String = " AM"
+
+}
