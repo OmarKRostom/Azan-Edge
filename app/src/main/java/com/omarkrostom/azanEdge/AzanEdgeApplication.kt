f650f57b929eade9549a62439ab186fc191ff94c
Omar Khairy Rostom <o.rostom.93@gmail.com>
2018-05-20 22:27:19 +0200

    Added widget classes and error reporting tool

--- app/src/main/java/com/omarkrostom/azanEdge/AzanEdgeApplication.kt
+++ app/src/main/java/com/omarkrostom/azanEdge/AzanEdgeApplication.kt
@@ -1,38 +1,46 @@
 package com.omarkrostom.azanEdge
 
 import android.app.Application
-import android.content.ComponentName
 import android.content.IntentFilter
-import android.content.SharedPreferences
-import android.os.Handler
-import android.preference.PreferenceManager
-import android.widget.Toast
-import com.omarkrostom.azanEdge.broadcastReceivers.EdgeSinglePlusProvider
-import com.omarkrostom.azanEdge.networking.PrayersApiManager
-import com.omarkrostom.azanEdge.networking.models.PrayerResponse
-import com.omarkrostom.azanEdge.utils.get
-import com.omarkrostom.azanEdge.utils.set
-import com.omarkrostom.azanEdge.utils.setPrayerTimes
-import com.samsung.android.sdk.look.cocktailbar.SlookCocktailManager
+import com.bugsnag.android.Bugsnag
+import com.omarkrostom.azanEdge.widgetProviders.AzanEdgeHorizontalProvider
+import com.omarkrostom.azanEdge.widgetProviders.AzanEdgeSinglePlusProvider
 
 class AzanEdgeApplication : Application() {
 
     private val EDGE_PLUS_BROADCAST_RECEIVER = "com.samsung.android.cocktail.v2.action.COCKTAIL_UPDATE"
 
-    private val mEdgeSinglePlusProvider = EdgeSinglePlusProvider()
+
+    private val AZAN_EDGE_HORIZONTAL_BROADCAST_RECEIVER = "android.appwidget.action.APPWIDGET_UPDATE"
+
+    private val mEdgeSinglePlusProvider = AzanEdgeSinglePlusProvider()
+    private val mAzanEdgeHorizontalProvider = AzanEdgeHorizontalProvider()
 
     override fun onCreate() {
         super.onCreate()
 
-        /*Register EdgeSinglePlusProvider and PrayerTimesProvider*/
+        /*Initialize Error Reporting tool*/
+        initializeErrorReportingTool()
+
+        /*Register AzanEdgeSinglePlusProvider and PrayerTimesProvider*/
         registerReceiver(mEdgeSinglePlusProvider, IntentFilter(EDGE_PLUS_BROADCAST_RECEIVER))
+
+        /*Register AzanEdgeHorizontalProvider*/
+        registerReceiver(mAzanEdgeHorizontalProvider, IntentFilter(AZAN_EDGE_HORIZONTAL_BROADCAST_RECEIVER))
     }
 
     override fun onTerminate() {
         super.onTerminate()
 
-        /*Unregister EdgeSinglePlusProvider and PrayerTimesProvider*/
+        /*Unregister AzanEdgeSinglePlusProvider and PrayerTimesProvider*/
         unregisterReceiver(mEdgeSinglePlusProvider)
+
+        /*Unregister AzanEdgeSinglePlusProvider and PrayerTimesProvider*/
+        unregisterReceiver(mAzanEdgeHorizontalProvider)
+    }
+
+    private fun initializeErrorReportingTool() {
+        Bugsnag.init(this)
     }
 
 }
