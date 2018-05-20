d3cbc1a6b614b52482d0cb95436ab46cbc68e927
Omar Khairy Rostom <o.rostom.93@gmail.com>
2018-05-20 22:33:53 +0200

    Updated settings activity with hijri calendar

--- app/src/main/java/com/omarkrostom/azanEdge/activities/SettingsActivity.kt
+++ app/src/main/java/com/omarkrostom/azanEdge/activities/SettingsActivity.kt
@@ -1,15 +1,20 @@
 package com.omarkrostom.azanEdge.activities
 
 import android.annotation.SuppressLint
-import android.content.ComponentName
+import android.app.Activity
+import android.appwidget.AppWidgetManager
+import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
-import android.preference.PreferenceManager
-import android.support.v7.app.AppCompatActivity
+import android.provider.Settings
+import android.support.v7.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.CompoundButton
 import android.widget.Toast
@@ -17,28 +22,26 @@ import com.afollestad.materialdialogs.MaterialDialog
 import com.google.android.gms.location.*
 import com.omarkrostom.azanEdge.Config
 import com.omarkrostom.azanEdge.Constants
-import com.omarkrostom.azanEdge.broadcastReceivers.EdgeSinglePlusProvider
-import com.omarkrostom.azanEdge.networking.PrayersApiManager
-import com.omarkrostom.azanEdge.networking.models.PrayerResponse
-import com.omarkrostom.azanEdge.utils.PermissionDispatcher
-import com.omarkrostom.azanEdge.utils.get
-import com.omarkrostom.azanEdge.utils.set
-import com.omarkrostom.azanEdge.utils.setPrayerTimes
 import com.omarkrostom.azanEdge.R
-import com.samsung.android.sdk.look.cocktailbar.SlookCocktailManager
+import com.omarkrostom.azanEdge.networking.models.PrayerResponse
+import com.omarkrostom.azanEdge.networking.models.ReverseGeocodedObject
+import com.omarkrostom.azanEdge.networking.models.ReverseGeocodedObjectResponse
+import com.omarkrostom.azanEdge.networking.networkManagers.PrayersApiManager
+import com.omarkrostom.azanEdge.networking.networkManagers.PrayersApiManager.getLocationFormattedAddress
+import com.omarkrostom.azanEdge.utils.*
+import com.samsung.android.sdk.look.Slook
 import kotlinx.android.synthetic.main.activity_settings.*
-import java.util.*
 
-class SettingsActivity : AppCompatActivity(),
-        SharedPreferences.OnSharedPreferenceChangeListener,
-        PrayersApiManager.OnPrayerTimesApiListener {
+
+class SettingsActivity : BaseActivity(),
+        PrayersApiManager.OnPrayerTimesApiListener, PrayersApiManager.OnFormattedLocationApiListener {
 
     private lateinit var mLocationRequest: LocationRequest
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var mLocationCallback: LocationCallback
     private lateinit var mLocation: Location
-    private lateinit var mPreferenceManager: SharedPreferences
-    private lateinit var mCocktailIds: IntArray
+
+    private var mAppWidgetId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,20 +50,104 @@ class SettingsActivity : AppCompatActivity(),
 
     override fun onStart() {
         super.onStart()
+        initializeSlookInstance()
+        setCurrentWidgetId()
         createLocationRequestInstance()
         createFusedLocationProviderInstance()
         createLocationCallbackInstance()
         setActionListeners()
-        setSharedPreferencesInstance()
+        initializePreferenceManager()
         updateLocationInButton()
         updateMethodInButton()
         updateHourModeSwitch()
+        updatePreviewTimes()
+        onChooseLocationClicked()
+    }
+
+    override fun onSuccess(prayerResponse: PrayerResponse) {
+        /* Save response to preferences */
+        mPreferenceManager.set(Constants.FAJR_PRAYER, prayerResponse.data.prayerTimings.fajr)
+        mPreferenceManager.set(Constants.ZUHR_PRAYER, prayerResponse.data.prayerTimings.zuhr)
+        mPreferenceManager.set(Constants.ASR_PRAYER, prayerResponse.data.prayerTimings.asr)
+        mPreferenceManager.set(Constants.MAGHRIB_PRAYER, prayerResponse.data.prayerTimings.maghrib)
+        mPreferenceManager.set(Constants.ISHA_PRAYER, prayerResponse.data.prayerTimings.isha)
+
+        /* Set Hijri Date */
+        tv_day.visibility = View.VISIBLE
+        tv_day.text = prayerResponse.data.prayersDate.hijriDate
+                .get("day").toString().replace("\"", "")
+
+        tv_month.visibility = View.VISIBLE
+        tv_month.text = prayerResponse.data.prayersDate.hijriDate
+                .getAsJsonObject("month").get("en").toString().replace("\"", "")
+
+        tv_year.visibility = View.VISIBLE
+        tv_year.text = prayerResponse.data.prayersDate.hijriDate
+                .get("year").toString().replace("\"", "")
+
+        updateWidgets()
+        updatePreviewTimes()
+    }
+
+    override fun onError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onLocationSetSuccess(reverseGeocodedObjectResponse: ReverseGeocodedObjectResponse) {
+        mPreferenceManager.set(Constants.APP_ADDRESS, reverseGeocodedObjectResponse
+                .addressComponents[0].formattedAddress)
+        mPreferenceManager.set(Constants.APP_LAT, mLocation.latitude.toString())
+        mPreferenceManager.set(Constants.APP_LONG, mLocation.longitude.toString())
+
+        updateLocationInButton()
+    }
+
+    override fun onLocationSetError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.btn_save -> finishAndSave()
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setCurrentWidgetId() {
+        val intent = intent
+        val extras = intent.extras
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID)
+        }
     }
 
     private fun setActionListeners() {
-        fl_location_btn.setOnClickListener(this::onChooseLocationClicked)
-        fl_azan_method_btn.setOnClickListener(this::onChooseAzanMethodClicked)
+        ll_location_btn.setOnClickListener(this::onChooseLocationClicked)
+        ll_azan_method_btn.setOnClickListener(this::onChooseAzanMethodClicked)
         sc_time_format.setOnCheckedChangeListener(this::onPrayerTimingChanged)
+        ll_manual_adjustments_title.setOnClickListener(this::onManualAdjustmentsClicked)
+        ll_samsung_enabled.setOnClickListener(this::onSamsungCompatibleClicked)
+    }
+
+    private fun onManualAdjustmentsClicked(view: View) {
+        val intent = Intent(this, ManualTimesActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onSamsungCompatibleClicked(view: View) {
+        val message: String = if (mSlookInstance.isFeatureEnabled(Slook.COCKTAIL_PANEL)) {
+            getString(R.string.samsung_edge_panel_enabled)
+        } else {
+            getString(R.string.samsung_edge_panel_disabled)
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     private fun onPrayerTimingChanged(buttonView: CompoundButton, isChecked: Boolean) {
@@ -74,6 +161,9 @@ class SettingsActivity : AppCompatActivity(),
                 mPreferenceManager.set(Constants.HOUR_MODE, Constants.TWENTY_FOUR_HOUR)
             }
         }
+
+        updateWidgets()
+        updatePreviewTimes()
     }
 
     private fun onChooseAzanMethodClicked(view: View) {
@@ -90,11 +180,6 @@ class SettingsActivity : AppCompatActivity(),
                             dialog.selectedIndex)
 
                     updateMethodInButton()
-
-                    /* Show that method updated sucessfully */
-                    Toast.makeText(this,
-                            getString(R.string.method_updated_successfully),
-                            Toast.LENGTH_LONG).show()
                 }
                 .positiveText(android.R.string.ok)
                 .show()
@@ -104,7 +189,7 @@ class SettingsActivity : AppCompatActivity(),
         return mPreferenceManager.get(Constants.AZAN_METHOD_INDEX, Constants.DEFAULT_METHOD_INDEX) as Int
     }
 
-    private fun onChooseLocationClicked(view: View) {
+    private fun onChooseLocationClicked(view: View = View(this)) {
         if (PermissionDispatcher.checkLocationPermission(this)) {
             if (PermissionDispatcher.checkLocationEnabled(this)) {
                 getLocationUpdates()
@@ -139,33 +224,18 @@ class SettingsActivity : AppCompatActivity(),
                 Looper.myLooper())
     }
 
-    private fun setSharedPreferencesInstance() {
-        mPreferenceManager = PreferenceManager.getDefaultSharedPreferences(this)
-        mPreferenceManager.registerOnSharedPreferenceChangeListener(this)
-    }
-
     private fun setLocationToPreferences() {
-        val geoCoder = Geocoder(this, Locale.getDefault())
-        val addresses = geoCoder.getFromLocation(mLocation.latitude, mLocation.longitude, 1)
-        mPreferenceManager.set(Constants.APP_LAT, mLocation.latitude.toLong())
-        mPreferenceManager.set(Constants.APP_LONG, mLocation.longitude.toLong())
-        mPreferenceManager.set(Constants.APP_CITY, addresses[0].adminArea)
-        mPreferenceManager.set(Constants.APP_COUNTRY, addresses[0].countryName)
-
-        updateLocationInButton()
-
-        /* Show that location updated sucessfully */
-        Toast.makeText(this,
-                getString(R.string.location_updated_successfully),
-                Toast.LENGTH_LONG).show()
+        getLocationFormattedAddress(
+                mLocation.longitude.toString(),
+                mLocation.latitude.toString(),
+                this::onLocationSetSuccess,
+                this::onLocationSetError
+        )
     }
 
     private fun updateLocationInButton() {
-        tv_location_result.text = "${
-        mPreferenceManager.get(Constants.APP_COUNTRY, Constants.DEFAULT_COUNTRY)
-        }, ${
-        mPreferenceManager.get(Constants.APP_CITY, Constants.DEFAULT_CITY)
-        }"
+        tv_location_result.text = "${mPreferenceManager.get(Constants.APP_ADDRESS,
+                Constants.DEFAULT_ADDRESS)}"
     }
 
     private fun updateMethodInButton() {
@@ -212,20 +282,12 @@ class SettingsActivity : AppCompatActivity(),
 
     private fun getPrayerTimesFromApi(sharedPreferences: SharedPreferences = mPreferenceManager) {
         PrayersApiManager.getPrayerTimesFromApi(
-                sharedPreferences.get(Constants.APP_COUNTRY, Constants.DEFAULT_COUNTRY).toString(),
-                sharedPreferences.get(Constants.APP_CITY, Constants.DEFAULT_CITY).toString(),
+                sharedPreferences.get(Constants.APP_LAT, Constants.DEFAULT_LAT).toString(),
+                sharedPreferences.get(Constants.APP_LONG, Constants.DEFAULT_LONG).toString(),
                 ((sharedPreferences.get(Constants.AZAN_METHOD_INDEX, Constants.DEFAULT_METHOD_INDEX) as Int) + 1).toString(),
                 this::onSuccess,
                 this::onError
         )
-    }
-
-    private fun updateWidget() {
-        mCocktailIds = SlookCocktailManager.getInstance(this).getCocktailIds(
-                ComponentName(this, EdgeSinglePlusProvider::class.java)
-        )
-
-        setPrayerTimes(packageName, mCocktailIds, this)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -234,30 +296,40 @@ class SettingsActivity : AppCompatActivity(),
         when (requestCode) {
             Config.LOCATION_PERMISSION ->
                 if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocationUpdates()
+                    onChooseLocationClicked()
                 } else {
-                    Toast.makeText(this, getString(R.string.please_allow_location), Toast.LENGTH_LONG).show()
+                    showPermissionDialog()
                 }
         }
     }
 
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
-        getPrayerTimesFromApi(sharedPreferences)
+    private fun showPermissionDialog() {
+        AlertDialog.Builder(this)
+                .setTitle(getString(R.string.permissions_disabled))
+                .setMessage(getString(R.string.permissions_disabled_string))
+                .setPositiveButton(R.string.go_to_settings, { _, _ ->
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", packageName, null))
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                })
+                .show()
     }
 
-    override fun onSuccess(prayerResponse: PrayerResponse) {
-        /* Save response to preferences */
-        mPreferenceManager.set(Constants.FAJR_PRAYER, prayerResponse.data.prayerTimings.fajr)
-        mPreferenceManager.set(Constants.ZUHR_PRAYER, prayerResponse.data.prayerTimings.zuhr)
-        mPreferenceManager.set(Constants.ASR_PRAYER, prayerResponse.data.prayerTimings.asr)
-        mPreferenceManager.set(Constants.MAGHRIB_PRAYER, prayerResponse.data.prayerTimings.maghrib)
-        mPreferenceManager.set(Constants.ISHA_PRAYER, prayerResponse.data.prayerTimings.isha)
-
-        updateWidget()
+    private fun updatePreviewTimes() {
+        tv_fajr_prayer.text = getAzanTiming(mPreferenceManager, Constants.FAJR_PRAYER)
+        tv_zuhr_prayer.text = getAzanTiming(mPreferenceManager, Constants.ZUHR_PRAYER)
+        tv_asr_prayer.text = getAzanTiming(mPreferenceManager, Constants.ASR_PRAYER)
+        tv_maghreb_prayer.text = getAzanTiming(mPreferenceManager, Constants.MAGHRIB_PRAYER)
+        tv_isha_prayer.text = getAzanTiming(mPreferenceManager, Constants.ISHA_PRAYER)
     }
 
-    override fun onError(error: String) {
-        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    private fun finishAndSave(): Boolean {
+        val resultValue = Intent()
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId)
+        setResult(Activity.RESULT_OK, resultValue)
+        finish()
+        return true
     }
 
 }
