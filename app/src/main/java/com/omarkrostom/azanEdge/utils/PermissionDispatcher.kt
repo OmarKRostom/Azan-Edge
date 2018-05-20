84f9019fbce5cdb10b816e529c09cd597e0c280b
Omar Khairy Rostom <o.rostom.93@gmail.com>
2018-05-20 22:34:59 +0200

    Removed unused code

--- app/src/main/java/com/omarkrostom/azanEdge/utils/PermissionDispatcher.kt
+++ app/src/main/java/com/omarkrostom/azanEdge/utils/PermissionDispatcher.kt
@@ -35,13 +35,6 @@ object PermissionDispatcher {
 
     }
 
-    fun isServiceRunning(context: Context, serviceClass: Class<*>): Boolean {
-        val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        return manager.getRunningServices(Integer.MAX_VALUE).any {
-            serviceClass.name == it.service.className
-        }
-    }
-
     fun checkLocationEnabled(context: Context): Boolean =
             (context.getSystemService(Context.LOCATION_SERVICE) as LocationManager)
                     .isProviderEnabled(LocationManager.NETWORK_PROVIDER)
